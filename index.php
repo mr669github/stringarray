dc90be19d9542c4f930de6b1af9a8f0f45a73abf
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-09-28 19:50:55 -0400

    added 5String functions so far

--- index.php
+++ index.php
@@ -1,19 +1,56 @@
+
 <?php
 $obj = new main();
+$text = "WEB DEVELOPMENT STRING ARRAYS";
+
 class main
 {
-	public function __construct()
-	{
-	echo 'Hello I\'m an object! </br>';
+    public function __construct()
+    {  
+    echo 'Hello I\'m an object! </br>';
+     echo 'My Input string is: WEB DEVELOPMENT STRING ARRAYS</br>';
+    stringFunctions::myStringfunc();
+    }   
+    public function __destruct()
+    {
+     echo '</br> I\'m Done';
+    } 
+}
+class stringFunctions
+{
 
-	}
-	public function __destruct()
-	{
-	echo '</br> I\'m Done';
-	}
+   static public function printThis($text)
+   {
+   print($text);
+   echo '</br><hr>';
+   }
+   static public function myStringfunc()
+   {    
+   $text="WEB DEVELOPMENT STRING ARRAYS";
+   echo '<h2>STRING LENGTH FUNCTION DEMO</h2>';
+   $var= strlen($text);
+   stringFunctions::printThis($var);
+   
+   echo '<h2>REPLACE SEARCH STRING "ARRAY" WITH "REPLACED" DEMO</h2>';
+    $var=str_replace("ARRAYS", "REPLACED", $text);
+   stringFunctions::printThis($var);
+
+   echo '<h2>RETURN SUBSTRING FUNCTION</h2>';
+   $var=substr($text,4,11);
+   stringFunctions::printThis($var);
+
+   echo '<h2>WORD COUNT FUNCTION</h2>';
+   $var=str_word_count($text);
+   stringFunctions::printThis($var);
+
+    echo '<h2>REVERSE STRING FUNCTION</h2>';
+    $var=strrev($text);
+    stringFunctions::printThis($var);
+}  
 
 
-} 
 
+
+}
 
 ?>
