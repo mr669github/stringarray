6142a6cb220751f2188a6f5a13897eaed4fa4e65
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-09-28 18:46:10 -0400

    added onject instantiation  constructor destructor

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$obj = new main();
+class main
+{
+	public function __construct()
+	{
+	echo 'Hello I\'m an object! </br>';
+
+	}
+	public function __destruct()
+	{
+	echo '</br> I\'m Done';
+	}
+
+
+} 
+
+
+?>
