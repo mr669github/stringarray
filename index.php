2bc9d587ce250f0547a25ee084287f569678cbea
monisha ramesh <mr669@afsconnect2.njit.edu>
2017-09-28 22:02:19 -0400

    added necessary headings

--- index.php
+++ index.php
@@ -8,7 +8,6 @@ class main
 public function __construct()
 {  
 echo 'Hello I\'m an object! </br>';
-echo 'My Input string is: WEB DEVELOPMENT STRING ARRAY</br>';
 stringFunctions::myStringfunc();
 stringFunctions::myArrayfunc();
 }   
@@ -30,9 +29,12 @@ static public function printThisarr($text)
 print_r($text);
 echo '</br><hr>';
 }
+//STRING FUNCTIONS START HERE
 static public function myStringfunc()
 {    
 $text="Web Development String Array";
+echo '<h1>***10 STRING FUNCTIONS***</br></br></h1>';
+echo 'My Input string is: WEB DEVELOPMENT STRING ARRAY</br>';
 echo '<h2>STRING LENGTH FUNCTION DEMO</h2>';
 $var= strlen($text);
 stringFunctions::printThis($var);
@@ -73,62 +75,63 @@ echo '<h2>POSITION OF SUBSTRING "WEB"  FUNCTION</h2>';
 $var=strpos($text,'Web');
 stringFunctions::printThisarr($var);
 }
-
+//ARRAY FUNCTIONS START HERE
 static public function myArrayfunc()
 {
 $array=array(1,2,3,4,5,6,7);
+echo '<h1>***10 ARRAY FUNCTIONS***</br></br></h1>';
 echo '<h2>ARRAY COUNT FUNCTION</h2>';
 echo 'INPUT ARRAY=A,A,B,C,D,D</br>';
 $array1=array(A,A,B,C,D,D);
 $var=array_count_values($array1);
 stringFunctions::printThisarr($var);
 
-  echo '<h2>ARRAY SEARCH FUNCTION</h2>';
-  echo 'INPUT ARRAY=A,A,B,C,D,D</br>Search for C';
-  $var=array_search('C',$array1);
-  stringFunctions::printThisarr($var);
+echo '<h2>ARRAY SEARCH FUNCTION</h2>';
+echo 'INPUT ARRAY=A,A,B,C,D,D</br>Search for C</br>';
+$var=array_search('C',$array1);
+stringFunctions::printThisarr($var);
 
-  echo '<h2>ARRAY POP FUNCTION</h2>';
-  echo 'INPUT ARRAY=1,2,3,4,5,6,7</br>';
-  $var=array_pop($array);
-  stringFunctions::printThisarr($var);
+echo '<h2>ARRAY POP FUNCTION</h2>';
+echo 'INPUT ARRAY=1,2,3,4,5,6,7</br>';
+$var=array_pop($array);
+stringFunctions::printThisarr($var);
 
-  echo '<h2>ARRAY SLICE  FUNCTION</h2>';
-  echo 'INPUT ARRAY=A,B,C,D,E</br>';
-  $array2=array(A,B,C,D,E);
-  $var=array_slice($array2,4);
-  stringFunctions::printThisarr($var);
+echo '<h2>ARRAY SLICE  FUNCTION</h2>';
+echo 'INPUT ARRAY=A,B,C,D,E</br>';
+$array2=array(A,B,C,D,E);
+$var=array_slice($array2,4);
+stringFunctions::printThisarr($var);
 
-  echo '<h2>ARRAY REVERSE  FUNCTION</h2>';
-  echo 'INPUT ARRAY=A,B,C,D,E</br>';
-  $array2=array(A,B,C,D,E);
-  $var=array_reverse($array2);
-  stringFunctions::printThisarr($var);
-  
-   echo '<h2>ARRAY FLIP FUNCTION WHERE KEY VALUE PAIRS ARE FLIPPED</h2>';
-   echo 'INPUT ARRAY=A,B,C,D,E</br>';
-   $array2=array(A,B,C,D,E);
-   $var=array_flip($array2);
-   stringFunctions::printThisarr($var);
+echo '<h2>ARRAY REVERSE  FUNCTION</h2>';
+echo 'INPUT ARRAY=A,B,C,D,E</br>';
+$array2=array(A,B,C,D,E);
+$var=array_reverse($array2);
+stringFunctions::printThisarr($var);
 
-   echo '<h2>ARRAY UNIQUE FUNCTION(Removes redundant value)</h2>';
-   echo 'INPUT ARRAY=A,A,B,C,D,D</br>';
-   $array1=array(A,A,B,C,D,D);
-   $var=array_unique($array1);
-   stringFunctions::printThisarr($var);
+echo '<h2>ARRAY FLIP FUNCTION WHERE KEY VALUE PAIRS ARE FLIPPED</h2>';
+echo 'INPUT ARRAY=A,B,C,D,E</br>';
+$array2=array(A,B,C,D,E);
+$var=array_flip($array2);
+stringFunctions::printThisarr($var);
 
-   echo '<h2>ARRAY PRODUCT FUNCTION</h2>';
-   echo 'INPUT ARRAY=1,2,3,4,5,6,7</br>';
-   $var=array_product($array);
-   stringFunctions::printThisarr($var);
+echo '<h2>ARRAY UNIQUE FUNCTION(Removes redundant value)</h2>';
+echo 'INPUT ARRAY=A,A,B,C,D,D</br>';
+$array1=array(A,A,B,C,D,D);
+$var=array_unique($array1);
+stringFunctions::printThisarr($var);
 
-   echo '<h2>ARRAY SUM FUNCTION</h2>';
-   echo 'INPUT ARRAY=1,2,3,4,5,6,7</br>';
-   $var=array_sum($array);
-   stringFunctions::printThisarr($var);
+echo '<h2>ARRAY PRODUCT FUNCTION</h2>';
+echo 'INPUT ARRAY=1,2,3,4,5,6,7</br>';
+$var=array_product($array);
+stringFunctions::printThisarr($var);
 
-   echo '<h2>ARRAY VALUES  FUNCTION</h2>';
-   echo 'INPUT ARRAY: SIZE=>XL, COLOR=PINK</br>';
+echo '<h2>ARRAY SUM FUNCTION</h2>';
+echo 'INPUT ARRAY=1,2,3,4,5,6,7</br>';
+$var=array_sum($array);
+stringFunctions::printThisarr($var);
+
+echo '<h2>ARRAY VALUES  FUNCTION</h2>';
+echo 'INPUT ARRAY: SIZE=>XL, COLOR=PINK</br>';
    $array3=array("size"=>"XL","color"=>"pink");
    $var=array_values($array3);
    stringFunctions::printThisarr($var);
